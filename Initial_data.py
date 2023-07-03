3d92b2267900f3984ba21534e40475a455045618
Margarita Medvedeva <margosh_ik@mail.ru>
2023-07-03 09:33:47 +0300

    Vk_Bot_version 3

--- Initial_data.py
+++ Initial_data.py
@@ -1,11 +1,11 @@
-community_token = 'vk1.a.CQo89mnFZs7G2uQCFkG4fd8KrbtnI-9QVsrM6c0wmrBxsYWWIjzC0bB5E59D_N8DxoB8GNY7a2H-HL5wTzwRu8dfYpIJBEQT7YAAVBqasLVngDRTxfRc1lEhoMi3--bwqM3ZXOXEGIjJBJrWOyH1wAMnoei2pG7bYmMgsGpbC0bA7UTxFRCdJtpbNwpmY33eK1xKxJCmbV8q6CeQEeh96g'
-user_token = 'vk1.a.ALzFg7EfRIsG9pKTpGDmMjm3wjIvoGb1RRi24a5YvAA5BMYJbflkjaBu-2HbNPWl5-86r0zWadRls3QKMeU3pMSpmhA4D6eOmP_tY-bxja6AlN3P6oQ1DbnHYBefcLmozi7OD-7C25y8CXWqD6Ek1PR4jiDHB7E7ke4KAwlVe04HofMzooEoVZa04XjEAS9YMk-GSPCjxXRCO_OLTBUwwg'
-community_id = '218241665'
+community_token = ''
+user_token = ''
+community_id = ''
 
-Username = 'postgres'
-Password = 'Okeanelzi12'
-Host = 'localhost'
-Database_vk = 'postgres'
+Username = ''
+Password = ''
+Host = ''
+Database_vk = ''
 Port = 5432
 
 API_VERSION = '5.131'
